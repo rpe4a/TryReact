b2318bdd3c00891999c536e1b09e15d7054a48c8
Михаил Ю. Плюснин <plyusnin@iac.local>
2017-08-28 17:27:03 +0500

    add eslinter

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './scss/main.css';
 
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo1' />
           <h2>Welcome to React!</h2>
         </div>
-        <p className="App-intro">
-          <i className='fa fa-id-badge'></i>To get started, edit <code>src/App.js</code> and save to reload.
+        <p className='App-intro'>
+          <i className='fa fa-pencil'></i>To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
