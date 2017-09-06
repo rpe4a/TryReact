f1638a47b56e321841ae99449f68d069f9a3081e
Михаил Ю. Плюснин <plyusnin@iac.local>
2017-09-06 13:04:56 +0500

    curse

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './scss/main.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./scss/main.css";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo1' />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo1" />
           <h2>Welcome to React!</h2>
         </div>
-        <p className='App-intro'>
-          <i className='fa fa-pencil'></i>To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro">
+          <i className="fa fa-pencil" />To get started, edit{" "}
+          <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
